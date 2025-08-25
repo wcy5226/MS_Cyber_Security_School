fefe1f477d4ad75b9cce6fe2e9930f485effbb21
wcy5226 <wcy5226@naver.com>
2025-08-25 09:23:48 +0900

    0825
    0825

---
+++ 0825.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int age, score;
+    printf("Your age : "); scanf("%d", &age);
+    printf("Your score : "); scanf("%d", &score);
+
+    printf("Age : %d\n", age);
+    printf("Score : %d\n", score);
+}
